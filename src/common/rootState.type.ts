3ebec070685c6b01f509174861f510d422ef9804
AzureAD\koterahiroki <kotera.hiroki@itfllc.co.jp>
2023-03-13 08:12:14 +0900

    RTK関連ファイル削除

--- src/common/rootState.type.ts
+++
@@ -1,4 +0,0 @@
-import { store } from "../features/store";
-
-
-export type RootState = ReturnType<typeof store.getState>;
--- src/features/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { todoSlice } from "../features/todoSlice";
-
-export const store = configureStore({
-  reducer: todoSlice.reducer,
-});
