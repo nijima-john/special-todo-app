d18e6ddec84dcd21ca5f8249b735eb24e24b91a7
AzureAD\koterahiroki <kotera.hiroki@itfllc.co.jp>
2023-03-18 08:21:29 +0900

    削除機能追加

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,10 +1,15 @@
-import { useSelector } from 'react-redux';
+import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
 import { type RootState } from '../app/store';
-import { type Todo } from "../features/todo/todoSlice"
+import { remove, type Todo } from "../features/todo/todoSlice"
 
 export const List: React.FunctionComponent = () => {
 
+  const dispatch = useDispatch();
   const todos = useSelector((state: RootState) => state.todos.todos)
+  const removeTodos = (id: string): void => {
+    dispatch(remove(id))
+  }
 
   return (
     <>
@@ -14,8 +19,8 @@ export const List: React.FunctionComponent = () => {
           <div key={todo.id}>
             <h3>{todo.isCompleted ? "完了" : "未完了"}</h3>
             <div>内容: {todo.content}</div>
+            <Button variant="contained" onClick={() => { removeTodos(todo.id); }}>削除</Button>
             <div>{todo.isCompleted}</div>
-
           </div>
         )
       })}
