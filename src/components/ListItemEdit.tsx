362be4b1dcb9703398033cefcd88231b822928e0
AzureAD\koterahiroki <kotera.hiroki@itfllc.co.jp>
2023-04-14 10:58:41 +0900

    デザイン修正

--- src/components/ListItemEdit.tsx
+++ src/components/ListItemEdit.tsx
@@ -11,10 +11,9 @@ export const ListItemEdit: React.FunctionComponent<Props> = (props) => {
     const { content, handleChange, editTodo } = props;
     return (
         <div>
-            <h2>編集してください</h2>
             <TextField type="text" value={content} name="content"
                 onChange={handleChange} />
-            <Button style={{ marginLeft: '25px', marginTop: '5px' }} variant="contained" onClick={() => { editTodo(); }}>更新する</Button>
+            <Button style={{ width: "50%", marginLeft: "20%", marginTop: "5%" }} variant="contained" onClick={() => { editTodo(); }}>更新</Button>
         </div>
     )
 }
